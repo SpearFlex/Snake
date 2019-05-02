1e5193212b43449181a76cd1005fcdb3c7cb7cfc
SpearFlex <aleksandr-razumov2004@mail.ru>
2019-05-02 12:07:38 +0300

    function Draw

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+        }
+        static void Draw(int x,int y,char sym )
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write( sym );
         }
     }
 }
